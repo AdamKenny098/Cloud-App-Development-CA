086700a2b035c5ac4439733b9350ebf83ce1e8b8
Adam Kenny <adamtkenny@gmail.com>
2025-11-03 11:51:27 +0000

    Initial Commit and Project Initialized
    Initialized CDK TypeScript project and configured dependencies for serverless app lab

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Cloud-App-Development-CA
 Assignment for Cloud App Development 2025-2026
+
+This is a blank project for CDK development with TypeScript.
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+## Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `npx cdk deploy`  deploy this stack to your default AWS account/region
+* `npx cdk diff`    compare deployed stack with current state
+* `npx cdk synth`   emits the synthesized CloudFormation template
---
+++ test/cloud-app-development-ca.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudAppDevelopmentCa from '../lib/cloud-app-development-ca-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloud-app-development-ca-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudAppDevelopmentCa.CloudAppDevelopmentCaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
